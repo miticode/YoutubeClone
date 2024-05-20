ea322bb3761bf44b6b16b93776f4352c7ddefdc9
Le Huynh Minh Tri <lehuynhminhtri09082003@gmail.com>
2024-05-20 08:21:06 +0700

    addft

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./pages/home";
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+  ]);
+  return  <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import Content from "../../component/content"
+import Header from "../../component/header"
+import "./index.scss"
+
+function Home() {
+  return (
+    <div className="home">
+        <Header/>
+        <Content/>
+    </div>
+  )
+}
+
+export default Home

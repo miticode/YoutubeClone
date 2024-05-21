bd1457e91209f81ed2f0a9e9890359dbf95c9090
Le Huynh Minh Tri <lehuynhminhtri09082003@gmail.com>
2024-05-22 00:58:09 +0700

    add time

--- src/component/video/index.jsx
+++ src/component/video/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./index.scss";
 import { Link } from "react-router-dom";
+import { formatDistanceToNow } from "date-fns";
 
 
 function Video() {
@@ -30,6 +31,36 @@ function Video() {
     getApi();
   }, []);
 
+
+  
+
+  const convertISO8601ToDuration = (isoDuration) => {
+    const regex = /PT(\d+H)?(\d+M)?(\d+S)?/;
+    const matches = isoDuration.match(regex);
+
+    let hours = matches[1] ? parseInt(matches[1].replace('H', '')) : 0;
+    let minutes = matches[2] ? parseInt(matches[2].replace('M', '')) : 0;
+    let seconds = matches[3] ? parseInt(matches[3].replace('S', '')) : 0;
+
+    let duration = '';
+    if (hours > 0) {
+      duration += `${hours}:`;
+    }
+    duration += `${minutes.toString().padStart(2, '0')}:`;
+    duration += `${seconds.toString().padStart(2, '0')}`;
+    return duration;
+  };
+  const formatViewCount = (viewCount) => {
+    if (viewCount >= 1_000_000_000) {
+      return (viewCount / 1_000_000_000).toFixed(1) + 'B';
+    } else if (viewCount >= 1_000_000) {
+      return (viewCount / 1_000_000).toFixed(1) + 'M';
+    } else if (viewCount >= 1_000) {
+      return (viewCount / 1_000).toFixed(1) + 'K';
+    }
+    return viewCount.toString();
+  };
+ 
   return (
     <div className="video-grid">
         {api.length > 0 ? (
@@ -38,12 +69,17 @@ function Video() {
             <Link to={`https://www.youtube.com/watch?v=${videodata.id}`} key={index} className="video-item">
               <div className="thumbnail">
                 <img src={videodata.snippet.thumbnails.medium.url} alt={videodata.snippet.title} />
+                <span>{convertISO8601ToDuration(videodata.contentDetails.duration)}</span>
               </div>
               <div className="video-info">
-                <img src="" alt="" />
+          
                 <h4 className="video-title">{videodata.snippet.title}</h4>
                 <p className="channel-title">{videodata.snippet.channelTitle}</p>
-                <p className="view-count">{videodata.statistics.viewCount} views</p>
+                <span className="video-meta">
+                <p className="view-count">{formatViewCount(videodata.statistics.viewCount)} views</p>
+                <p className="published-time">{formatDistanceToNow(new Date(videodata.snippet.publishedAt))} ago</p>
+                </span>
+               
               </div>
             </Link>
         
